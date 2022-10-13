f6b149f322b978130ae2e6497ab31c3bc94a42dc
Adil Ansari <ansari.adil20@gmail.com>
2022-10-13 12:18:16 -0700

    feat: Auth client setup

--- lib/tigris.ts
+++ lib/tigris.ts
@@ -1,30 +1,32 @@
 import { Tigris, TigrisClientConfig } from '@tigrisdata/core'
 
-if (!process.env.TIGRIS_HOST) {
-  throw new Error('Please add TIGRIS_HOST env variable to .env.local')
+if (!process.env.TIGRIS_URI) {
+  throw new Error('TIGRIS_URI env variable is missing in .env.local ')
 }
 
-let tigris: Tigris
-const tigrisHost: string = process.env.TIGRIS_HOST as string
-const clientConfig: TigrisClientConfig = getClientConfig(tigrisHost)
-
-function getClientConfig (inputUrl: string): TigrisClientConfig {
-  const noAuthRequired: boolean = inputUrl == null ||
-    inputUrl === 'undefined' ||
-    inputUrl.length === 0 ||
-    inputUrl.includes('localhost') ||
-    inputUrl.includes('127.0.0.1') ||
-    inputUrl.includes('0.0.0.0')
-
-  if (noAuthRequired) {
-    return { serverUrl: inputUrl, insecureChannel: true }
-  } else {
-    throw new Error('auth setup not implemented')
-  }
-}
+const tigrisUri: string = process.env.TIGRIS_URI as string
 
 declare global {
   var tigrisClient: Tigris
+}
+
+let tigris: Tigris
+let clientConfig: TigrisClientConfig
+
+if (process.env.NODE_ENV === 'production' || requiresAuth(tigrisUri)) {
+  if (!process.env.TIGRIS_CLIENT_ID || !process.env.TIGRIS_CLIENT_SECRET) {
+    throw new Error(
+      'TIGRIS_CLIENT_ID and TIGRIS_CLIENT_SECRET missing in .env.local')
+  }
+  const clientId = process.env.TIGRIS_CLIENT_ID as string
+  const clientSecret = process.env.TIGRIS_CLIENT_SECRET as string
+  clientConfig = {
+    serverUrl: tigrisUri,
+    clientId: clientId,
+    clientSecret: clientSecret
+  }
+} else {
+  clientConfig = { serverUrl: tigrisUri, insecureChannel: true }
 }
 
 // TigrisClient can be global to re-use connections in development
@@ -40,3 +42,13 @@ if (process.env.NODE_ENV === 'production') {
 // export to share client across modules
 export default tigris
 
+function requiresAuth (inputUrl: string): boolean {
+  const isLocalDev: boolean = inputUrl == null ||
+    inputUrl === 'undefined' ||
+    inputUrl.length === 0 ||
+    inputUrl.includes('localhost') ||
+    inputUrl.includes('127.0.0.1') ||
+    inputUrl.includes('0.0.0.0')
+
+  return !isLocalDev
+}
