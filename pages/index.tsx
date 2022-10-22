22bdf991b47a928d68282ecf8e387937c1deae41
Taha Khan <tkhan@tigrisdata.com>
2022-10-21 23:11:02 -0700

    Added comments and removed a sub folder under components. (#18)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next';
 import Image from 'next/image';
 import Head from 'next/head';
 import React, { useEffect, useState } from 'react';
-import EachTodo from '../components/LoaderWave/EachToDo';
-import LoaderWave from '../components/LoaderWave/LoaderWave';
+import EachTodo from '../components/EachToDo';
+import LoaderWave from '../components/LoaderWave';
 import { TodoItem } from '../lib/schema';
 import styles from '../styles/Home.module.css';
 
@@ -13,14 +13,25 @@ const Home: NextPage = () => {
 
   // Todo list array which displays the todo items
   const [todoList, setTodoList] = useState<TodoItem[]>([]);
+
+  // Loading and Error flags for the template
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
+  // This is use to animate the input text field
   const [wiggleError, setWiggleError] = useState(false);
+
+  // Two separate views. 1. List view for todo items & 2. Search result view
   type viewModeType = 'list' | 'search';
   const [viewMode, setViewMode] = useState<viewModeType>('list');
 
   // Fetch Todo List
+  /*
+   'fetchListItems' is the first method that's called when the component is mounted from the useEffect below.
+   This sets some of the state like 'isLoading' and 'isError' before it fetches for data from the endpoint defined under 'pages/api/items/index'.
+   The api endpoint returns a json with the key 'result' and a status 200 if successful or returns a status 500 along with the 'error' key.
+   If the 'result' key is present we safely set the 'todoList'.
+  */
   const fetchListItems = () => {
     setIsLoading(true);
     setIsError(false);
@@ -48,6 +59,10 @@ const Home: NextPage = () => {
   }, []);
 
   // Add a new todo-item
+  /*
+  'addToDoItem' takes the 'textInput' state, creates a 'TodoItem' & converts it to a JSON.
+  Sends it over as body payload to the api endpoint; which is how the api expects and is defined in 'pages/api/items' 'POST' switch.
+  */
   const addToDoItem = () => {
     if (queryCheckWiggle()) {
       return;
@@ -65,6 +80,10 @@ const Home: NextPage = () => {
   };
 
   // Delete Todo-item
+  /*
+  'deleteTodoItem' requires an id value of the TodoItem. When the user presses the 'delete'(cross) button from a TodoItem, this method is invoked.
+  It calls the endpoint 'api/item/<id>' with the 'DELETE' method. Read the method 'handleDelete' under pages/api/item/[id]' to learn more how the api handles deletion.
+  */
   const deleteTodoItem = (id?: number) => {
     setIsLoading(true);
 
@@ -81,6 +100,10 @@ const Home: NextPage = () => {
   };
 
   // Update Todo-item (mark complete/incomplete)
+  /*
+  'updateTodoItem' takes the TodoItem object, inverts the 'completed' boolean and calls the same endpoint as 'deletion' but with a different method 'PUT'.
+  Navigate to 'api/item/[id]' and read more how the api handles updates under the 'handlePut' method.
+  */
   const updateTodoItem = (item: TodoItem) => {
     item.completed = !item.completed;
     setIsLoading(true);
@@ -99,6 +122,10 @@ const Home: NextPage = () => {
   };
 
   // Search query
+  /*
+  'searchQuery' method takes the state from 'textInput' and send it over to the 'api/items/search' endpoint via a query param 'q'.
+  The response is the same as the response from "fetch('/api/items')", an array of TodoItems if successful. 
+  */
   const searchQuery = () => {
     if (queryCheckWiggle()) {
       return;
@@ -119,6 +146,10 @@ const Home: NextPage = () => {
   };
 
   // Util search query/input check
+  /*
+  The is a helper util method, that validtes the input field via a regex and returns a true or false.
+  This also wiggles the text input if the regex doesn't find any match.
+  */
   const queryCheckWiggle = () => {
     const result: RegExpMatchArray | null = textInput.match('^\\S.{0,100}$');
     if (result === null) {
