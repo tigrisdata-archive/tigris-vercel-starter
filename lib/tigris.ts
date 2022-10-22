a1e18ecb51cd42d1905a1eec5faa5a539981ce0d
Taha Khan <tkhan@tigrisdata.com>
2022-10-21 18:44:14 -0700

    Tk/prettier lint (#17)
    * Added prettier and husky
    
    * Added prettier ignore and prettierrc
    
    * Added husky pre-commit hook. Prettified code.
    
    * added the 'prettier' at the end in eslintrc so it that lint doesn't conflict with prettier. Updated the useState to 'textInput'.
    
    For running husky and enabling prettier and lint check before committing we need to:
    
    npm install
    npm prepare
    npx husky add .husky/pre-commit "npm run lint && npm run prettier"

--- lib/tigris.ts
+++ lib/tigris.ts
@@ -1,37 +1,37 @@
-import { DB, Tigris, TigrisClientConfig } from '@tigrisdata/core'
-import { DB_NAME } from './schema'
+import { DB, Tigris, TigrisClientConfig } from '@tigrisdata/core';
+import { DB_NAME } from './schema';
 
 if (!process.env.TIGRIS_URI) {
-  throw new Error('Cannot find TIGRIS_URI environment variable ')
+  throw new Error('Cannot find TIGRIS_URI environment variable ');
 }
 
-const tigrisUri = process.env.TIGRIS_URI
-const clientConfig: TigrisClientConfig = { serverUrl: tigrisUri }
+const tigrisUri = process.env.TIGRIS_URI;
+const clientConfig: TigrisClientConfig = { serverUrl: tigrisUri };
 
 if (process.env.TIGRIS_CLIENT_ID) {
-  clientConfig.clientId = process.env.TIGRIS_CLIENT_ID
+  clientConfig.clientId = process.env.TIGRIS_CLIENT_ID;
 }
 if (process.env.TIGRIS_CLIENT_SECRET) {
-  clientConfig.clientSecret = process.env.TIGRIS_CLIENT_SECRET
+  clientConfig.clientSecret = process.env.TIGRIS_CLIENT_SECRET;
 }
 
 declare global {
   // eslint-disable-next-line no-var
-  var tigrisDb: DB
+  var tigrisDb: DB;
 }
 
-let tigrisDb: DB
+let tigrisDb: DB;
 
 if (process.env.NODE_ENV === 'production') {
   if (!global.tigrisDb) {
-    const tigrisClient = new Tigris(clientConfig)
-    global.tigrisDb = tigrisClient.getDatabase(DB_NAME)
+    const tigrisClient = new Tigris(clientConfig);
+    global.tigrisDb = tigrisClient.getDatabase(DB_NAME);
   }
-  tigrisDb = global.tigrisDb
+  tigrisDb = global.tigrisDb;
 } else {
-  const tigrisClient = new Tigris(clientConfig)
-  tigrisDb = tigrisClient.getDatabase(DB_NAME)
+  const tigrisClient = new Tigris(clientConfig);
+  tigrisDb = tigrisClient.getDatabase(DB_NAME);
 }
 
 // export to share DB across modules
-export default tigrisDb
+export default tigrisDb;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,226 +1,213 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 import Image from 'next/image';
-import Head from 'next/head'
-import React, { useEffect, useState } from 'react'
-import EachTodo from '../components/LoaderWave/EachToDo'
-import LoaderWave from '../components/LoaderWave/LoaderWave'
-import { TodoItem } from '../lib/schema'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import React, { useEffect, useState } from 'react';
+import EachTodo from '../components/LoaderWave/EachToDo';
+import LoaderWave from '../components/LoaderWave/LoaderWave';
+import { TodoItem } from '../lib/schema';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
+  // This is the input field
+  const [textInput, setTextInput] = useState('');
 
+  // Todo list array which displays the todo items
   const [todoList, setTodoList] = useState<TodoItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
 
-  //
-  const [querySearch, setQuerySearch] = useState('');
   const [wiggleError, setWiggleError] = useState(false);
-  type viewModeType = 'list' | 'search'
+  type viewModeType = 'list' | 'search';
   const [viewMode, setViewMode] = useState<viewModeType>('list');
 
   // Fetch Todo List
-  const fetchListItems = ()=>{
-    
+  const fetchListItems = () => {
     setIsLoading(true);
     setIsError(false);
 
-    fetch("/api/items")
-    .then(response => response.json())
-    .then(data => 
-      {
-        setIsLoading(false)
-        if(data.result){  
-          setViewMode('list')
-          setTodoList(data.result)
+    fetch('/api/items')
+      .then(response => response.json())
+      .then(data => {
+        setIsLoading(false);
+        if (data.result) {
+          setViewMode('list');
+          setTodoList(data.result);
+        } else {
+          setIsError(true);
         }
-        else {
-          setIsError(true)
-        }
-      }
-    )
-    .catch(() => {
-      setIsLoading(false)
-      setIsError(true)
-    });
-  }
-
+      })
+      .catch(() => {
+        setIsLoading(false);
+        setIsError(true);
+      });
+  };
 
   // Load the initial list of todo-items
-  useEffect(()=>{
-    fetchListItems()
-  },[]);
+  useEffect(() => {
+    fetchListItems();
+  }, []);
 
-  
   // Add a new todo-item
   const addToDoItem = () => {
-
-    if(queryCheckWiggle()){
+    if (queryCheckWiggle()) {
       return;
     }
     setIsLoading(true);
 
-    fetch("/api/items", {
-      method:'POST',
-      body:JSON.stringify({text:querySearch, completed:false})
-    })
-    .then(() => 
-      {
-        setIsLoading(false);
-        setQuerySearch('');
-        fetchListItems();
-      }
-    )
-
-  }
-
+    fetch('/api/items', {
+      method: 'POST',
+      body: JSON.stringify({ text: textInput, completed: false })
+    }).then(() => {
+      setIsLoading(false);
+      setTextInput('');
+      fetchListItems();
+    });
+  };
 
   // Delete Todo-item
-  const deleteTodoItem = (id?:number) => {
-
+  const deleteTodoItem = (id?: number) => {
     setIsLoading(true);
 
-    fetch("/api/item/"+id, {
-      method:'DELETE',
-    })
-    .then(() => 
-      {
-        setIsLoading(false);
-        if(viewMode == 'list'){
-          fetchListItems();
-        }
-        else{
-          searchQuery();
-        }
+    fetch('/api/item/' + id, {
+      method: 'DELETE'
+    }).then(() => {
+      setIsLoading(false);
+      if (viewMode == 'list') {
+        fetchListItems();
+      } else {
+        searchQuery();
       }
-    )
-
-  }
-
+    });
+  };
 
   // Update Todo-item (mark complete/incomplete)
-  const updateTodoItem = (item:TodoItem) => {
-
+  const updateTodoItem = (item: TodoItem) => {
     item.completed = !item.completed;
     setIsLoading(true);
-  
-    fetch("/api/item/"+item.id, {
-      method:'PUT',
-      body:JSON.stringify(item)
-    })
-    
-    .then(() => 
-      {
-        setIsLoading(false);
-        if(viewMode == 'list'){
-          fetchListItems();
-        }
-        else{
-          searchQuery();
-        }
-        
+
+    fetch('/api/item/' + item.id, {
+      method: 'PUT',
+      body: JSON.stringify(item)
+    }).then(() => {
+      setIsLoading(false);
+      if (viewMode == 'list') {
+        fetchListItems();
+      } else {
+        searchQuery();
       }
-    )
-
-  }
-
-
+    });
+  };
 
   // Search query
   const searchQuery = () => {
-
-    if(queryCheckWiggle()){
+    if (queryCheckWiggle()) {
       return;
     }
     setIsLoading(true);
 
-    fetch(`/api/items/search?q=${encodeURI(querySearch)}`, {
-      method:'GET',
+    fetch(`/api/items/search?q=${encodeURI(textInput)}`, {
+      method: 'GET'
     })
-    .then(response => response.json())
-    .then((data) => 
-      {
+      .then(response => response.json())
+      .then(data => {
         setIsLoading(false);
-        if(data.result){ 
-          setViewMode('search')
-          setTodoList(data.result)
+        if (data.result) {
+          setViewMode('search');
+          setTodoList(data.result);
         }
-      }
-    )
-
-  }
-
+      });
+  };
 
   // Util search query/input check
   const queryCheckWiggle = () => {
-
-    const result: RegExpMatchArray | null = querySearch.match('^\\S.{0,100}$');
-    if(result === null){
+    const result: RegExpMatchArray | null = textInput.match('^\\S.{0,100}$');
+    if (result === null) {
       setWiggleError(true);
       return true;
     }
     return false;
-  }
+  };
 
-  useEffect(()=>{
-    if(!wiggleError){
-      return; 
+  useEffect(() => {
+    if (!wiggleError) {
+      return;
     }
-    const timeOut = setTimeout(()=>{
+    const timeOut = setTimeout(() => {
       setWiggleError(false);
-    },500)
+    }, 500);
 
     return () => clearTimeout(timeOut);
-    
-  },[wiggleError])
+  }, [wiggleError]);
 
-
-  
   return (
     <div>
       <Head>
         <title>Todo App using Next.js + Tigris</title>
         <meta name="description" content="Tigris app tutorial" />
       </Head>
-      
+
       <div className={styles.container}>
-        
         <h2>Sample Todo app using Next.js and Tigris</h2>
 
         {/* Search Header */}
         <div className={styles.searchHeader}>
-          <input className={`${styles.searchInput} ${(wiggleError) ? styles.invalid : ''}`} value={querySearch} onChange={e=>{setWiggleError(false); setQuerySearch(e.target.value)}} placeholder='Type an item to add or search'/>
+          <input
+            className={`${styles.searchInput} ${wiggleError ? styles.invalid : ''}`}
+            value={textInput}
+            onChange={e => {
+              setWiggleError(false);
+              setTextInput(e.target.value);
+            }}
+            placeholder="Type an item to add or search"
+          />
           <button onClick={addToDoItem}>Add</button>
           <button onClick={searchQuery}>Search</button>
         </div>
 
         {/* Results section */}
         <div className={styles.results}>
-         
           {/* Loader, Errors and Back to List mode */}
           {isError && <p className={styles.errorText}>Something went wrong.. </p>}
-          {isLoading && <LoaderWave/>}
-          {viewMode=='search' && <button className={styles.clearSearch} onClick={()=>{setQuerySearch(''); fetchListItems()}}>Go back to list</button>}
+          {isLoading && <LoaderWave />}
+          {viewMode == 'search' && (
+            <button
+              className={styles.clearSearch}
+              onClick={() => {
+                setTextInput('');
+                fetchListItems();
+              }}
+            >
+              Go back to list
+            </button>
+          )}
 
           {/* Todo Item List */}
-          {(todoList.length < 1) ? <p className={styles.noItems}>{(viewMode=='search') ? 'No items found.. ' : 'Add a todo by typing in the field above and hit Add!' }</p> :
-           (<ul>
-              { todoList.map((each)=>{
-                return(<EachTodo key={each.id} toDoItem={each} deleteHandler={deleteTodoItem} updateHandler={updateTodoItem}/>)
+          {todoList.length < 1 ? (
+            <p className={styles.noItems}>
+              {viewMode == 'search' ? 'No items found.. ' : 'Add a todo by typing in the field above and hit Add!'}
+            </p>
+          ) : (
+            <ul>
+              {todoList.map(each => {
+                return (
+                  <EachTodo
+                    key={each.id}
+                    toDoItem={each}
+                    deleteHandler={deleteTodoItem}
+                    updateHandler={updateTodoItem}
+                  />
+                );
               })}
-            </ul>)
-          }
-  
-            
+            </ul>
+          )}
         </div>
 
-        <a href="https://tigrisdata.com/"><Image src="/tigris_logo.svg" alt="Tigris Logo" width={100} height={100}/></a>
-
+        <a href="https://tigrisdata.com/">
+          <Image src="/tigris_logo.svg" alt="Tigris Logo" width={100} height={100} />
+        </a>
       </div>
-       
     </div>
-  )
-}
+  );
+};
 
-export default Home
- 
+export default Home;
