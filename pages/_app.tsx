172d3f76f95e134bcf07f98a3a63863bfe7d4340
Tim Raderschad <tim.raderschad@gmail.com>
2022-11-25 10:14:41 +0100

    feat: clean up code and use SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
