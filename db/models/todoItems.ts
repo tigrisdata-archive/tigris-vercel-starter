7e6036ce196b97937b01094ced3a2aad67afb77b
Adil Ansari <adil@tigrisdata.com>
2023-01-24 10:48:33 -0800

    using model without tigris collection type

--- db/models/todoItems.ts
+++ db/models/todoItems.ts
@@ -1,7 +1,7 @@
 import { Field, PrimaryKey, TigrisCollection, TigrisCollectionType, TigrisDataTypes } from '@tigrisdata/core';
 
 @TigrisCollection('todoItems')
-export class TodoItem implements TigrisCollectionType {
+export class TodoItem {
   @PrimaryKey(TigrisDataTypes.INT32, { order: 1, autoGenerate: true })
   id!: number;
 
